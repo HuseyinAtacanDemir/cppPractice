d1024d169a92a2677a5feeebebcefe0322031aae
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-02-17 19:38:03 +0300

    string compare substr find replace find first of

--- practice14.cpp
+++ practice14.cpp
@@ -2,22 +2,27 @@
 #include <string>
 //string methods modifiers
 int main(){
-    std::string greeting = "Hello";
-    greeting += " there";
-    std::cout << greeting << std::endl;
-    greeting.append("!");
-    std::cout << greeting << std::endl;
-    greeting.insert(3,"   ");
-    std::cout << greeting << std::endl;
-    greeting.erase(3,3);
-    std::cout << greeting << std::endl;
-    greeting.pop_back();
-    std::cout << greeting << std::endl;
-    greeting.replace(0,4,"Heaven");
+    std::string greeting = "WHAT THE FUCK!";
+
+    greeting.replace(greeting.find("FUCK"), 4, "****");
+
     std::cout << greeting << std::endl;
 
-    std::cout << greeting.size() << std::endl;
-    std::cout << greeting.size() << std::endl;
-    std::cout << greeting.length() << std::endl;
+    std::cout << greeting.substr(5,3) << std::endl;
 
+    std::cout << greeting.find_first_of("AEIOU") << std::endl;
+    //std::string.find_first_of() returns an unsigned long -1 if not found
+    //that's why we get a huge number, because of overflow... npos
+    
+    if(greeting.find_first_of("é") == -1) std::cout << "NOT FOUND!" << std::endl;
+
+    if(greeting == "Hello"){ 
+        std::cout << "equals" << std::endl;
+    }else{
+        std::cout << "not equals" << std::endl;
+    }
+    if(greeting == "WHAT THE ****!") std::cout << "equals" << std::endl;
+
+    //greeting.compare("string here") .compare is not that important for cpp
+    
 }
